ceb0bdc19104f92e25a6115a877b1338cf59f1ae
Ashish <noisy@kali.noisy>
2021-09-16 21:35:05 +0530

    Added a new route

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/add')
+def add_new():
+    return render_template('form.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
