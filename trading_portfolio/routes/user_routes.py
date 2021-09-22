714930163514255096eb257dc7ba585d8399de32
Ashish <noisy@kali.noisy>
2021-09-22 12:32:06 +0530

    Added new route for submission of data

--- trading_portfolio/routes/user_routes.py
+++ trading_portfolio/routes/user_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from trading_portfolio.database.database import Database
+from trading_portfolio.database.database import DataBase
 from trading_portfolio.api.coin import KuCoin
 
 def create_blueprint(cluster):
@@ -19,12 +19,29 @@ def create_blueprint(cluster):
 
         return render_template('user/form.html', currency_pairs=currency_pairs)
 
-    @user.route('/add/<string:currency_pair>', methods=['GET', 'POST'])
+    @user.route('/add/<string:currency_pair>', methods=['GET'])
     def currency_info(currency_pair):
         currency_pairs = kucoin.get_currencies()
         tickers = kucoin.get_each_currency(currency_pair)
         current_price = tickers['price']
 
         return render_template('user/form.html', currency_pairs=currency_pairs, currency_pair=currency_pair, current_price=current_price)
+
+    @user.route('/add/submit', methods=['POST'])
+    def add_currency():
+        coin_name = request.form["coin-name"]
+        coin_quantity = request.form["coin-quantity"]
+        coin_buy_price = request.form["buy-price"]
+        
+
+        data = {
+            "coin name": coin_name,
+            "coin quantity": coin_quantity,
+            "coin buy price": coin_buy_price   
+        }
+
+        DataBase.add_pair(data)
+
+        return redirect(url_for('user.add_new'))
 
     return user
