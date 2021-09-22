ad55388d6ed797f8255924b81d77221d68b91548
Ashish <noisy@kali.noisy>
2021-09-22 12:33:41 +0530

    Small Update

--- trading_portfolio/config.py
+++ trading_portfolio/config.py
@@ -3,24 +3,25 @@ from dotenv import load_dotenv
 
 BASE_URL = path.abspath(path.dirname(__file__))
 
-""" 
-    Loading all thw key value pair specified in
+"""
+    Loading all the key value pair specified in
     .env file into the environment
 """
+load_dotenv(path.join(BASE_URL,'.env'))
 
-load_dotenv(path.join(BASE_URL, '.env'))
+""" Keep all the configurations here"""
 
-""" Keep all the configurations here """
+
 class Config:
-    SECRET_KEY = environ.get('SECRET_KEY')
+    SECRET_KEY = environ.get("SECRET_KEY")
     TESTING = False
 
 class Development(Config):
     DEBUG = True
     ENV = 'development'
-    MONGO_URI = environ.get('DEV_MONGO_URI')
+    MONGO_URI = environ.get("DEV_MONGO_URI")
 
 class Production(Config):
     DEBUG = False
-    ENV = 'production'
-    MONGO_URI = environ.get('PROD_MONGO_URI')
+    ENV = "production"
+    MONGO_URI = environ.get("PROD_MONGO_URI")
