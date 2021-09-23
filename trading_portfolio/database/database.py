fdd6db1a49a5c507a2af57573c3edf92a9e26a58
Ashish <noisy@kali.noisy>
2021-09-24 00:00:39 +0530

    Added method for searching the database

--- trading_portfolio/database/database.py
+++ trading_portfolio/database/database.py
@@ -8,3 +8,7 @@ class DataBase:
     @staticmethod
     def add_pair(data):
         DataBase.db.info.insert_one(data)
+
+    @staticmethod
+    def get_pair(pair):
+        return DataBase.db.info.find_one({ "coin name": pair}, {"_id" : 0})
